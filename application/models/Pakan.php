c14c307c0a0bdd587e3458a4bf7f84b12b8c4d18
mariamarghareta <maria.212116120@gmail.com>
2018-06-09 18:08:59 +0700

    Trace data monitoring pakan

--- application/models/Pakan.php
+++ application/models/Pakan.php
@@ -134,6 +134,7 @@ class Pakan extends CI_Model
         $data_sekarang = $this->get($id);
         $lama = $data_sekarang[0]->stok;
         $sekarang = $lama - $jumlah;
+        echo $lama . "-" . $jumlah . "=" . $sekarang;
         if($sekarang < 0){
             $sekarang = 0;
         }
